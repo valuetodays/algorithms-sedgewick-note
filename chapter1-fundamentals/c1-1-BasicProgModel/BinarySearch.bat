9d949d993e7ec1612842ed30faf34c43bf5c4f1d
liulei <lei.liu@datatist.com>
2020-09-14 18:35:23 +0800

    add stdlib & BinarySearch

---
+++ chapter1-fundamentals/c1-1-BasicProgModel/BinarySearch.bat
@@ -0,0 +1,4 @@
+java -Dfile.encoding=UTF-8 -classpath ./target/classes;../../stdlib/target/classes BinarySearch ./target/classes/tinyW.txt
+
+
+REM java -Dfile.encoding=UTF-8 -classpath ../../stdlib/target/classes:./target/classes BinarySearch ./target/classes/tinyW.txt
